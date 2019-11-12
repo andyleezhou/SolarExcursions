8f8425625d279755ce93284710c7f3313bbc3626
Andy Zhou <andy.lee.zhou@gmail.com>
2019-11-12 14:50:08 -0800

    Updates

---
+++ precache-manifest.e63e9cc4005449c5d4a521047b30fac3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9baf2c6fb9e51c012cec924aabeaafeb",
+    "url": "/SolarExcursions/index.html"
+  },
+  {
+    "revision": "ff7a5c1e2ac0cc2d7129",
+    "url": "/SolarExcursions/static/js/2.4f66d4f8.chunk.js"
+  },
+  {
+    "revision": "481650f417497cf16c1b",
+    "url": "/SolarExcursions/static/js/main.f6b606b8.chunk.js"
+  },
+  {
+    "revision": "e918bd5bc60e4e69eb49",
+    "url": "/SolarExcursions/static/js/runtime-main.f6d6aba7.js"
+  }
+]);
